5119b6ed7c3d0464cc9998c2661b3f2bfd30c8aa
Mateus Coury <mateuscoury@gmail.com>
2020-10-23 17:43:38 -0300

    Criacao do arquivo de exercicio

---
+++ exercises/bloco4/dia_1/exercicio1.js
@@ -0,0 +1,29 @@
+/*  Primeiro exercicio
+
+let a = 5;
+let b = 2;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a / b) ;
+ 
+console.log(a % b) ;
+
+*/
+
+/* Segundo exercicio
+
+let a = 15 ;
+let b = 16 ;
+
+if(a > b){
+    console.log(a) ;
+}
+else{
+    console.log(b); 
+}
+*/
