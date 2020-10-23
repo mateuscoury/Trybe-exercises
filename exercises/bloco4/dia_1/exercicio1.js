1ab20814b06f6762cd2a1a4273d211be8ce10ce1
Mateus Coury <mateuscoury@gmail.com>
2020-10-23 18:26:41 -0300

    Exercicio de xadrez

--- exercises/bloco4/dia_1/exercicio1.js
+++ exercises/bloco4/dia_1/exercicio1.js
@@ -27,3 +27,73 @@ else{
     console.log(b); 
 }
 */
+
+/*  Terceiro exercicio
+
+let a = 2 ;
+let b = 5 ;
+let c = 4 ;
+
+
+if (a > b && a > c ) {
+    console.log("a e maior")
+}
+else if (b > a && b > c) {
+    console.log("b e maior")
+}
+else if( c > b && c > a) {
+    console.log("c e maior")
+}
+
+*/
+
+/* Quarto ex
+
+let a = -25
+
+if ( a > 0) {
+    console.log("valor positivo") ;
+}
+else if(a < 0){
+    console.log("valor negativo") ;
+}
+else {
+console.log("zero")
+}
+
+*/
+
+/* Sexto ex
+
+let nomedapeca = "a";
+
+
+switch (nomedapeca) {
+    case "a":
+        console.log("diagonal");
+        break;
+
+    case "b":
+        console.log("avancar")
+
+        break;
+
+    case "c":
+        console.log("qualquer direcao")
+        break;
+
+        default :
+        console.log("nao existe") ;
+        
+
+}
+
+*/
+
+
+
+
+
+
+
+
