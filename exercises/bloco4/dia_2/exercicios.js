1d65484347f5be087eb9ccb245ff37c317000f7e
Mateus Coury <mateuscoury@gmail.com>
2020-10-26 18:32:55 -0300

    Exercicios 1 e 2 prontos

---
+++ exercises/bloco4/dia_2/exercicios.js
@@ -0,0 +1,26 @@
+/* Imprimi valores
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let i = 0 ; i < numbers.length ; i++) {
+    console.log(numbers[i])
+}
+
+*/
+
+/*    soma dos numeros do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0
+
+for (let i = 0 ; i < numbers.length ; i++) {
+     
+ soma = soma + numbers[i]
+
+
+
+}
+
+console.log(soma)
+
+*/
