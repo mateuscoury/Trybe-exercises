8e5be38e6f3dc30da23a7129765e829126972dd0
Mateus Coury <mateuscoury@gmail.com>
2020-10-26 19:18:50 -0300

    Todos os exercicios finalizados

--- exercises/bloco4/dia_2/exercicios.js
+++ exercises/bloco4/dia_2/exercicios.js
@@ -74,5 +74,116 @@ for (let i = 0 ; i < numbers.length ; i++) {
 
 */
 
+/*   Calcula numeros impares
+     
+
+
+let numbers = [6, 10, 5, 18, 70, 4, 100, 2, 36, 28];
+
+let impares = 0;
+
+
+for (let i = 0 ; i < numbers.length ; i++) {
+    if(numbers[i] % 2 == 1){
+     
+        impares =  impares + 1
+
+
+}
+
+}
+
+if(impares === 0){
+    console.log("Nao tem numero impar")
+}
+else
+{
+
+console.log(impares)
+}
+
+
+*/
+
+/* numero maior
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+let numeromaior = 0
+
+for(let i = 0 ; i < numbers.length ; i ++) {
+    if(numbers[i] > numeromaior) {
+        numeromaior = numbers[i] ;
+    }
+   
+        
+    }
+   
+
+    console.log(numeromaior)
+
+    */
+
+
+   /* numero menor
+   
+    let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+   let numeromenor = 1000
+   
+   for(let i = 0 ; i < numbers.length ; i ++) {
+       if(numbers[i] < numeromenor) {
+           numeromenor = numbers[i] ;
+      
+    
+      
+        }
+      
+           
+       }
+       console.log(numeromenor)
+      
+   
+       */
+
+     
+     /* Criacao do array com 25 numeros
+     
+       let numbers = [] ; 
+     
+
+     for (let i = 0 ; i <= 25 ; i++){
+         numbers.push(i) ;
+     }
+
+     console.log(numbers) ;
+
+        
+    */
+
+   
+   /* Divisao dos numeros por 2
+    
+    
+    let numbers = [] ; 
+     
+    
+   
+    for (let i = 0 ; i <= 25 ; i++){
+       numbers.push(i) ;
+
+
+       console.log(numbers[i] / 2)
+
+
+     
+   }
+
+   */
+
+
+
+
+
