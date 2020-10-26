6ba8015b419644e09bca520506fb51a8f1b8603c
Mateus Coury <mateuscoury@gmail.com>
2020-10-26 18:42:39 -0300

    Exercicio 4 completo

--- exercises/bloco4/dia_2/exercicios.js
+++ exercises/bloco4/dia_2/exercicios.js
@@ -24,3 +24,55 @@ for (let i = 0 ; i < numbers.length ; i++) {
 console.log(soma)
 
 */
+
+
+/*  Media dos numeros
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0
+
+let media 
+
+for (let i = 0 ; i < numbers.length ; i++) {
+     
+ soma = soma + numbers[i] 
+
+ media = soma / numbers.length
+
+
+
+}
+
+console.log(media)
+
+*/
+
+/*   Exercicio 4
+
+let numbers = [5, 9, 3, 19, 70, 8, 2, 2, 5, 27];
+
+let soma = 0
+
+let media 
+
+for (let i = 0 ; i < numbers.length ; i++) {
+     
+ soma = soma + numbers[i] 
+
+ media = soma / numbers.length
+
+}
+
+ if( media > 20 ) {
+     console.log("valor maior que 20")
+
+ }
+ else
+ console.log("valor menor ou igual a 20")
+
+
+*/
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
