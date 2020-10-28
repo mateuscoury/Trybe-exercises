521aad45ded84471fbb0cae7501f547c8ed3270d
Mateus Coury <mateuscoury@gmail.com>
2020-10-28 18:33:06 -0300

    Primeira Parte finalizada

---
+++ exercises/bloco4/dia_4/exercicios.js
@@ -0,0 +1,74 @@
+
+/*  primeiro
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    
+  };
+
+  console.log("Bem Vinda" + " " + info.personagem)
+
+  */
+
+/* Segundo
+
+ let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente : "Sim"
+  };
+
+*/
+
+
+/* Terceiro
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorente : "Sim"
+  };
+
+  for (let key in info) {
+      console.log(key)
+  }
+
+  */
+
+ /* Quarto
+ 
+  let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorente : "Sim"
+  };
+
+  for (let key in info) {
+      console.log(info[key])
+  }
+
+*/
+
+/* Quinto
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorente : "Sim"
+}
+
+let info1 = {
+    personagem : "Tio Patinhas" ,
+    origem : "Christmas on Bear Mountain,Dell's Four Color Comicas #178 " ,
+    nota : "O último MacPatinhas" ,
+    recorrente : "Sim"
+}
+console.log(info)
+
+*/
