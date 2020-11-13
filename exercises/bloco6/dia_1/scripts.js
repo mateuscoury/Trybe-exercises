4bbda3951fcb0c10bc33f5c2f7c19a67defcc582
Mateus Coury <mateuscoury@gmail.com>
2020-11-13 18:22:30 -0300

    Forumulario em progresso

---
+++ exercises/bloco6/dia_1/scripts.js
@@ -0,0 +1,8 @@
+function estados(){
+    let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí"]
+    let pegaestado = document.querySelector("#estados")
+
+
+
+}
+estados()
