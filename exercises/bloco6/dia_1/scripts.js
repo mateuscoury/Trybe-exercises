c1a82baae53ec9aa9188154b42e9304664aac228
Mateus Coury <mateuscoury@gmail.com>
2020-11-13 19:58:40 -0300

    Formulario ze

--- exercises/bloco6/dia_1/scripts.js
+++ exercises/bloco6/dia_1/scripts.js
@@ -1,8 +1,13 @@
 function estados(){
-    let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí"]
-    let pegaestado = document.querySelector("#estados")
+    let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia"]
+    let el = document.getElementById('estados');
+    let zin = document.createElement("option")
 
+    
 
-
+    zin.text = "ze"
+    el.options.add(zin,1)
+    
 }
+
 estados()
