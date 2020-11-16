9be28b4f5b791cad5befcd868a59b16931d2256b
Mateus Coury <mateuscoury@gmail.com>
2020-11-16 19:09:15 -0300

    exercicios do sapo

--- exercises/bloco6/dia_1/scripts.js
+++ exercises/bloco6/dia_1/scripts.js
@@ -1,13 +1,11 @@
 function estados(){
+
     let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia"]
     let el = document.getElementById('estados');
-    let zin = document.createElement("option")
-
+    el.innerHTML = '';
     
-
-    zin.text = "ze"
-    el.options.add(zin,1)
-    
+     estados.forEach(element => {
+     el.innerHTML += ` <option value="${element}">${element}</option>`
+    //  //el.innerHTML += ' <option value="' + element + '">'+ element + '</option>'
+    // });
 }
-
-estados()
