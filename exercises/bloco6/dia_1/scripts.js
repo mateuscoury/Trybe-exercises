f9572c21a2b81072f50e9095670d7c232fec89a3
Mateus Coury <mateuscoury@gmail.com>
2020-11-16 20:13:43 -0300

    Quase lá

--- exercises/bloco6/dia_1/scripts.js
+++ exercises/bloco6/dia_1/scripts.js
@@ -1,13 +1,21 @@
-function estados(){
+
     let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia"]
-    let el = document.getElementById('estados');
-    let zin = document.createElement("option")
+    let el = document.querySelector('#estados');
+
+    for(let index = 0 ; index <estados.length ; index++){
+        
+        let option=document.createElement("option") ;
+        
+        option.innerText = estados[index] ;
+        
+        option.value = estados[index] ;
+     
+
+        el.appendChild(option) ;
+    }
+ 
+
+
+
 
     
-
-    zin.text = "ze"
-    el.options.add(zin,1)
-    
-}
-
-estados()
