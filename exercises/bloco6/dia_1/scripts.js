319dc278d569e60e4264abfcf9bc1e8382fbe9a9
Mateus Coury <mateuscoury@gmail.com>
2020-11-17 19:12:43 -0300

    feita a validacao da data inicio

--- exercises/bloco6/dia_1/scripts.js
+++ exercises/bloco6/dia_1/scripts.js
@@ -1,5 +1,5 @@
 
-    let estados = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia"]
+    let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"]
     let el = document.querySelector('#estados');
 
     for(let index = 0 ; index <estados.length ; index++){
@@ -14,6 +14,30 @@
         el.appendChild(option) ;
     }
  
+
+
+    
+    let button = document.querySelector("#buttonid")
+  
+
+    //Informacoes contidas dentro do evento pois so depois q o usuario clickar e que vai estar setados os valores
+    button.addEventListener("click",function (event){
+       
+        let data = document.querySelector("#datainicio").value
+        let array = data.split("/") //transforma a string passada em array e divide os indices pelo separador "/"
+        let dia = parseInt(array[0]) //trasnforma em int e seta o dia como o primeiro indice
+        let mes = parseInt(array[1])//trasnforma em int e seta o mes como o segundo indice
+        let ano = parseInt(array[2])//trasnforma em int e seta o ano como o terceiro indice
+        
+if(!(dia > 0 && dia <=31 && mes > 0 && mes <=12 && ano >0)){ //faz a validacao se tudo for diferente(!) e exibe a mensagem de alerta
+    alert("Erro")
+
+}
+
+    })
+    
+    
+
 
 
 
