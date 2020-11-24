8e487f6eb07c8d8e0a0052ea7a798d38d8d849a5
Mateus Coury <mateuscoury@gmail.com>
2020-11-24 18:57:43 -0300

    exercicio 4 terminado

--- exercises/bloco7/dia_2/exercicios.js
+++ exercises/bloco7/dia_2/exercicios.js
@@ -62,3 +62,48 @@ const orderModifier = (order) => {
 };
 
 orderModifier(order);
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+//1
+const addNewKey = (obj, key, value) => {
+  const ze = (obj[key] = value);
+  console.log(ze);
+};
+
+addNewKey(lesson2, 'turno', 'manhã');
+//2
+const lista = (objeto) => {
+  const listaobj = Object.keys(objeto);
+  console.log(listaobj);
+};
+console.log(lista(lesson3));
+//3
+
+const tamanhp = (objeto) => {
+  const arrayobj = Object.keys(objeto).length;
+  console.log(arrayobj);
+};
+console.log(tamanhp(lesson3));
+
+//4
+
+const valoresobj = (objeto) => {};
+console.log(tamanhp(lesson3));
--- exercises/bloco7/dia_2/tempCodeRunnerFile.js
+++ exercises/bloco7/dia_2/tempCodeRunnerFile.js
@@ -1,64 +1,39 @@
-const order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
-  address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
-  },
-  order: {
-    pizza: {
-      margherita: {
-        amount: 1,
-        price: 25,
-      },
-      pepperoni: {
-        amount: 1,
-        price: 20,
-      },
-    },
-    drinks: {
-      coke: {
-        type: 'Coca-Cola Zero',
-        price: 10,
-        amount: 1,
-      },
-    },
-    delivery: {
-      deliveryPerson: 'Ana Silveira',
-      price: 5,
-    },
-  },
-  payment: {
-    total: 60,
-  },
-};
-/* const customerInfo = (order) => {
-  const address = 'address';
-  const deliveryPerson = order.order.delivery.deliveryPerson;
-  const customerName = order['name'];
-  const customerPhone = order['phoneNumber'];
-  const street = order[address].street;
-  const number = order[address].number;
-  const apartment = order[address].apartment;
-
-  console.log(
-    `Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`
-  );
-}; 
-
-customerInfo(order); */
-
-const orderModifier = (order) => {
-  const newBuyer = (order.name = 'Luiz Silva');
-  const pizzas = Object.keys(order.order.pizza);
-  console.log(pizzas);
-  const drinks = order.order.drinks.coke.type;
-  const newTotal = (order.payment.total = '50');
-
-  console.log(
-    `Olá ${newBuyer}, o total do seu pedido de ${pizzas} e ${drinks} é R$ ${newTotal},00.`
-  );
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
-orderModifier(order);
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+//1
+const addNewKey = (obj, key, value) => {
+  const ze = (obj[key] = value);
+  console.log(ze);
+};
+
+addNewKey(lesson2, 'turno', 'manhã');
+//2
+const lista = (objeto) => {
+  const listaobj = Object.keys(objeto);
+  console.log(listaobj);
+};
+console.log(lista(lesson3));
+//3
+
+const tamanhp = (objeto) => {
+  const arrayobj = Object.entries(objeto).length;
+  console.log(arrayobj);
+};
+console.log(tamanhp(lesson3));
