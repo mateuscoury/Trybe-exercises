e32684c9d67f771fdf0a0569d9f8b104c285cc46
Mateus Coury <mateuscoury@gmail.com>
2020-11-24 18:29:53 -0300

    exercicio parte 1 terminado

--- exercises/bloco7/dia_2/script.js
+++ exercises/bloco7/dia_2/script.js
@@ -74,3 +74,67 @@ let keyss = Object.keys(obje); // pega as chaves dos objetos
 console.log(values);
 
 console.log(keyss);
+
+//Object.entries
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países); //retorna posicao e valor
+for (i in pairKeyValue) {
+  console.log('--------');
+  console.log('Pais:', pairKeyValue[i][0]);
+  console.log('Capital:', pairKeyValue[i][1]);
+}
+
+console.log(pairKeyValue);
+
+//Object.assign
+
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family);
+console.log(person);
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  child: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */
+
+//passando pra um objeto vazio dois outros objects sem modificalos futuramente
+
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({}, person, lastName);
+newPerson.name = 'Gilberto';
+console.log(Object.keys(person).includes('ze')); //pergunta se tem ze nas keys do objeto person
+
+console.log(newPerson);
+console.log(person);
