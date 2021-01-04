aaee71eb18223118e4530a34e1d046f003a71069
Mateus Coury <mateuscoury@gmail.com>
2021-01-04 19:40:34 -0300

    exercicio 3.1

--- exercises/bloco10/dia_1/myremovew.test.js
+++ exercises/bloco10/dia_1/myremovew.test.js
@@ -3,3 +3,6 @@ const myremovew = require("./myremovew")
 test("array esperado", () => {
   expect(myremovew([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
 })
+test("array esperado 1", () => {
+  expect(myremovew([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+})
